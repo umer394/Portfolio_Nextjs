6f12d598a633a7a65e889b373f8ecd52dc209cb4
umer <umershhzad7@gmail.com>
2024-10-04 04:29:37 +0500

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
 
         <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
         <p className="md:text-base text-sm md:font-normal text-white font-light">
-          Copyright © 2024 Adrian Hajdin
+          Copyright © 2024 Muhammad Umer
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
